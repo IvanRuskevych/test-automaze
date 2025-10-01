e99de6095658d79b9461e9ebfe026e7d4396ff8a
IvanRuskevych <fullstack.dev.goit@gmail.com>
2025-10-01 11:15:56 +0300

    feat(repository): add findManyWithFilters to taskRepository

--- packages/backend/src/repositories/task.ts
+++ packages/backend/src/repositories/task.ts
@@ -2,35 +2,41 @@ import { Prisma, Task } from '@prisma/client';
 import { prismaClient } from '~/prisma/client.js';
 
 export const taskRepository = {
-  create(input: Prisma.TaskCreateInput, args?: Omit<Prisma.TaskCreateArgs, 'data'>): Promise<Task> {
+  create: (input: Prisma.TaskCreateInput, args?: Omit<Prisma.TaskCreateArgs, 'data'>): Promise<Task> => {
     return prismaClient.task.create({
       data: input,
       ...args,
     });
   },
 
-  delete(taskId: string): Promise<Task> {
+  delete: (taskId: string): Promise<Task> => {
     return prismaClient.task.delete({
       where: { id: taskId },
     });
   },
 
-  findById(taskId: string, args?: Omit<Prisma.TaskFindUniqueArgs, 'where'>): Promise<Task> {
+  findById: (taskId: string, args?: Omit<Prisma.TaskFindUniqueArgs, 'where'>): Promise<Task> => {
     return prismaClient.task.findUniqueOrThrow({
       where: { id: taskId },
       ...args,
     });
   },
 
-  findMany(args?: Prisma.TaskFindManyArgs): Promise<Task[]> {
-    return prismaClient.task.findMany({ ...args });
+  findManyWithFilters: (
+    where: Prisma.TaskWhereInput,
+    orderBy?: Prisma.TaskOrderByWithRelationInput,
+    skip?: number,
+    take?: number,
+    args?: Omit<Prisma.TaskFindManyArgs, 'where' | 'orderBy' | 'skip' | 'take'>,
+  ): Promise<Task[]> => {
+    return prismaClient.task.findMany({ where, orderBy, skip, take, ...args });
   },
 
-  update(
+  update: (
     taskId: string,
     data: Prisma.TaskUpdateInput,
     args?: Omit<Prisma.TaskUpdateArgs, 'where' | 'data'>,
-  ): Promise<Task> {
+  ): Promise<Task> => {
     return prismaClient.task.update({
       where: { id: taskId },
       data,
