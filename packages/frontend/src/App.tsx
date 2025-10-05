af8348d80cdd36591d95261602a125ca248b8e0a
IvanRuskevych <fullstack.dev.goit@gmail.com>
2025-10-05 20:04:15 +0300

    feat(app): update app component

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,8 +1,32 @@
-import { TestComponent } from '~/components';
-import './App.css';
+import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { Home } from './pages/Home';
+
+const theme = createTheme({
+  palette: { mode: 'light' },
+});
 
 function App() {
-  return <TestComponent />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+      <CssBaseline />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </ThemeProvider>
+  );
 }
 
 export default App;
