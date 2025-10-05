df58ee955675a5030af971845492b9e74e73d837
IvanRuskevych <fullstack.dev.goit@gmail.com>
2025-10-05 19:40:33 +0300

    feat(apollo): configure Apollo Client and initialize in main.ts

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,12 +1,15 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import { ApolloProvider } from '@apollo/client';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from '~/App.tsx';
 
-import App from './App.tsx';
-import './index.css';
+import { apolloClient } from './apollo/client';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ApolloProvider client={apolloClient}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>,
 );
